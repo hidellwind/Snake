142ba79808b347836e71e079a97251a6e0d098fb
hidellwind <lanapavlovaxx@gmail.com>
2024-05-16 13:26:07 +0300

    добавить задание

---
+++ Source/Snake/SnakeElement.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SnakeElement.generated.h"
+
+UCLASS()
+class SNAKE_API ASnakeElement : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ASnakeElement() = delete;
+	ASnakeElement(const FObjectInitializer& ObjectInitializer);
+	UStaticMeshComponent* GetMesh() { return SnakeElementMesh; };
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* SnakeElementMesh;
+};
